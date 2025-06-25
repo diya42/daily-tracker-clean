746b0b0a2db5da69d53c92325e0c8b76ec5bbead
ashhh42 <23f2003305@ds.study.iitm.ac.in>
2025-06-25 16:59:27 +0530

    Fix: use pymysql instead of MySQLdb for SQLAlchemy connection

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Load DB URL from Railway or .env
 DATABASE_URL = os.getenv("DATABASE_URL").replace("mysql://", "mysql+pymysql://")
-
+#comment
 
 # Use pymysql for MySQL
 engine = create_engine(DATABASE_URL, echo=True)
